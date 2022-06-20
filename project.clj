0d8c1455749e2db47160d3742b40e5f099b8a4b8
gumberss <gustavomarinsuppi@gmail.com>
2022-06-20 19:23:40 -0300

    feat: adding carmine reference

--- project.clj
+++ project.clj
@@ -12,5 +12,6 @@
                  [com.stuartsierra/component.repl "1.0.0"]
                  [prismatic/schema "1.3.0"]
                  [com.novemberain/monger "3.1.0"]
-                 [com.datomic/datomic-free "0.9.5697"]]
+                 [com.datomic/datomic-free "0.9.5697"]
+                 [com.taoensso/carmine "3.1.0"]]
   :repl-options {:init-ns purchase-listinator.core})
