0eb4477daae3ff60fe757dcf5827cc51c73aa690
gumberss <gustavomarinsuppi@gmail.com>
2022-07-03 22:51:51 -0300

    test: purchase list logic and its schema

--- test/purchase_listinator/logic/purchase_list_test.clj
+++ test/purchase_listinator/logic/purchase_list_test.clj
@@ -1,13 +1,15 @@
 (ns purchase-listinator.logic.purchase-list-test
   (:require [clojure.test :refer :all]
             [matcher-combinators.test :refer [match?]]
-            [purchase-listinator.logic.purchase-list :as logic.purchase-list]))
+            [purchase-listinator.logic.purchase-list :as logic.purchase-list]
+            [schema.core :as s]))
 
 (deftest generate-new-test
-  (testing "Should generate a new default list with the correct named informed"
-    (is (match? {:id           uuid?
-                 :name        "Most beautiful purchase list"
-                 :enabled     true
-                 :in-progress false
-                 :products    []}
-                (logic.purchase-list/generate-new "Most beautiful purchase list")))))
+  (s/with-fn-validation
+    (testing "Should generate a new default list with the correct named informed"
+      (is (match? {:id          uuid?
+                   :name        "Most beautiful purchase list"
+                   :enabled     true
+                   :in-progress false
+                   :products    []}
+                  (logic.purchase-list/generate-new "Most beautiful purchase list"))))))
