84c7fc45449fdfe0b0528dd72a982c1055e75e67
gumberss <gustavomarinsuppi@gmail.com>
2022-10-08 18:03:04 -0300

    feat: adding indexes in mongo db, at this moment, the point index latlong. Also added the shopping-location->db adapter to make the index pattern

--- src/purchase_listinator/components/mongo.clj
+++ src/purchase_listinator/components/mongo.clj
@@ -1,7 +1,8 @@
 (ns purchase-listinator.components.mongo
   (:require [monger.core :as mg]
-            [com.stuartsierra.component :as component]
-            [monger.collection :as mc]))
+            [com.stuartsierra.component :as component]))
+
+(def indexes (vector purchase-listinator.dbs.mongo.shopping-location/indexes))
 
 (defrecord Mongo [config]
   component/Lifecycle
@@ -9,7 +10,8 @@
     (let [{{:keys [host port db-name]} :mongo} config
           conn (mg/connect {:host host :port port})
           db (mg/get-db conn db-name)]
-      (mc/ensure-index db "people" (array-map :latlong "2dsphere"))
+      (doseq [db-indexes indexes]
+        (db-indexes db))
       (assoc this
         :connection conn
         :db db)))
--- src/purchase_listinator/dbs/mongo/shopping_location.clj
+++ src/purchase_listinator/dbs/mongo/shopping_location.clj
@@ -1,12 +1,18 @@
 (ns purchase-listinator.dbs.mongo.shopping-location
   (:require [schema.core :as s]
             [purchase-listinator.models.internal.shopping-location :as models.internal.shopping-location]
+            [purchase-listinator.adapters.db.mongo.shopping-location :as adapters.db.mongo.shopping-location]
             [monger.collection :as mc]))
 
 (def collection :shopping-location-test)
 
+(defn indexes
+  [db]
+  (mc/ensure-index db collection (array-map :latlong "2dsphere")))
+
 (s/defn upsert :- models.internal.shopping-location/ShoppingLocation
   [{:keys [shopping-id] :as shopping-location} :- models.internal.shopping-location/ShoppingLocation
    {:keys [db]}]
-  (mc/update db collection {:shopping-id shopping-id} shopping-location {:upsert true})
+  (let [db-shopping-location (adapters.db.mongo.shopping-location/internal->db shopping-location)]
+    (mc/update db collection {:shopping-id shopping-id} db-shopping-location {:upsert true}))
   shopping-location)
