e5c6c70ca21cae517f56b8165f193661f3aa8790
Bruno do Nascimento Maciel <brunodonascimentomaciel@gmail.com>
2022-10-20 20:56:06 -0300

    configure test namespace

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+            :url  "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.11.1"]
                  [com.stuartsierra/component "1.1.0"]
                  [io.pedestal/pedestal.service "0.5.10"]
@@ -21,4 +21,5 @@
                  [clj-time "0.15.2"]
                  [funcool/cats "2.2.0"]
                  [nubank/matcher-combinators "3.5.0"]]
+  :test-paths ["test/unit"]
   :repl-options {:init-ns purchase-listinator.core})
