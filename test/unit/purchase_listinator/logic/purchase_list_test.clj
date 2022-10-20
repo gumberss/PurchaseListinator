cf53c5dc4ce141712b0331c6eb6b333d3820e8d9
Bruno do Nascimento Maciel <brunodonascimentomaciel@gmail.com>
2022-10-20 20:57:16 -0300

    use s/deftest instead of s/with-fn-validation

--- test/unit/purchase_listinator/logic/purchase_list_test.clj
+++ test/unit/purchase_listinator/logic/purchase_list_test.clj
@@ -2,13 +2,12 @@
   (:require [clojure.test :refer :all]
             [matcher-combinators.test :refer [match?]]
             [purchase-listinator.logic.purchase-list :as logic.purchase-list]
-            [schema.core :as s]))
+            [schema.test :as s]))
 
-(deftest generate-new-test
-  (s/with-fn-validation
-    (testing "Should generate a new default list with the correct named informed"
-      (is (match? {:id          uuid?
-                   :name        "Most beautiful purchase list"
-                   :enabled     true
-                   :in-progress false}
-                  (logic.purchase-list/generate-new "Most beautiful purchase list"))))))
+(s/deftest generate-new-test
+  (testing "Should generate a new default list with the correct named informed"
+    (is (match? {:id          uuid?
+                 :name        "Most beautiful purchase list"
+                 :enabled     true
+                 :in-progress false}
+                (logic.purchase-list/generate-new "Most beautiful purchase list")))))
