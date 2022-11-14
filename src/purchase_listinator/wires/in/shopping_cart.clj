0d93dd8a0956759929ba59129859dcd11163157b
Bruno do Nascimento Maciel <brunodonascimentomaciel@gmail.com>
2022-11-14 19:15:47 -0300

    remove old position property from wire and internal models

--- src/purchase_listinator/wires/in/shopping_cart.clj
+++ src/purchase_listinator/wires/in/shopping_cart.clj
@@ -5,14 +5,12 @@
   {:event-type   s/Str
    :shopping-id  s/Str
    :category-id  s/Str
-   :old-position s/Int
    :new-position s/Int})
 
 (s/defschema ReorderItemEvent
   {:event-type      s/Str
    :shopping-id     s/Str
    :item-id         s/Str
-   :old-position    s/Int
    :new-position    s/Int
    :new-category-id s/Str})
 
