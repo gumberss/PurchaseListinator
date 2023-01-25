e8840fd2708a201c307cba1f7e83108fb4e887f0
gumberss <gustavomarinsuppi@gmail.com>
2023-01-24 21:53:03 -0300

    test: creating the first integratoin test components

--- src/purchase_listinator/endpoints/http/purchase_list.clj
+++ src/purchase_listinator/endpoints/http/purchase_list.clj
@@ -13,7 +13,6 @@
             [purchase-listinator.misc.http :as misc.http]
             [cats.monad.either :refer :all]))
 
-
 (s/defn get-purchase-lists :- {:status s/Int
                                :body   [out.purchases-lists/PurchaseList]}
   [{{:keys [datomic]} :component}]
