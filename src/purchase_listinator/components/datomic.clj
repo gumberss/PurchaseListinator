69dc999e38a562f724661e18bca05b4b4993beaa
gumberss <gustavomarinsuppi@gmail.com>
2023-02-02 23:14:10 -0300

    feat: changing datomic to datahike

--- src/purchase_listinator/components/datomic.clj
+++ src/purchase_listinator/components/datomic.clj
@@ -1,6 +1,6 @@
 (ns purchase-listinator.components.datomic
   (:require [com.stuartsierra.component :as component]
-            [datomic.api :as d]
+            [datahike.api :as d]
             [purchase-listinator.dbs.datomic.purchase-list :as datomic.purchase-list]
             [purchase-listinator.dbs.datomic.purchase-category :as datomic.purchase-category]
             [purchase-listinator.dbs.datomic.purchase-item :as datomic.purchase-item]
@@ -8,8 +8,6 @@
             [purchase-listinator.dbs.datomic.shopping-event :as dbs.datomic.shopping-events]
             [purchase-listinator.dbs.datomic.shopping-category :as dbs.datomic.shopping-category]
             [purchase-listinator.dbs.datomic.shopping-item :as dbs.datomic.shopping-item]))
-
-#_(def db-uri "datomic:free://localhost:4334/datomic-component?password=datomic")
 
 (def schema (concat datomic.purchase-list/schema
                     datomic.purchase-category/schema
@@ -19,15 +17,19 @@
                     dbs.datomic.shopping-item/schema
                     dbs.datomic.shopping-category/schema))
 
+(def cfg {:store {:backend :mem
+                  :id      "default"}})
+
 (defn create-schema [conn]
-  @(d/transact conn schema))
+  (d/transact conn schema))
 
 (defrecord Datomic [config]
   component/Lifecycle
 
   (start [component]
-    (d/create-database (-> config :datomic :db-uri))
-    (let [conn (d/connect (-> config :datomic :db-uri))]
+    (try (d/create-database cfg)
+         (catch Exception _))
+    (let [conn (d/connect cfg)]
       (create-schema conn)
       (assoc component :connection conn)))
 
--- src/purchase_listinator/publishers/purchase_list_category.clj
+++ src/purchase_listinator/publishers/purchase_list_category.clj
@@ -9,11 +9,13 @@
   [category :- models.internal.purchase-category/PurchaseCategory
    {:keys [publish]}]
   (publish :purchase-listinator/purchase-list.category.deleted
-           (adapters.out.purchase-list-category-events/->CategoryDeletedEvent category (misc.date/numb-now) (misc.general/squuid))))
+           (adapters.out.purchase-list-category-events/->CategoryDeletedEvent category (misc.date/numb-now) (misc.general/squuid)))
+  category)
 
 (s/defn category-created
   [category :- models.internal.purchase-category/PurchaseCategory
    {:keys [publish]}]
   (publish :purchase-listinator/purchase-list.category.created
-           (adapters.out.purchase-list-category-events/->CategoryCreatedEvent category (misc.date/numb-now) (misc.general/squuid))))
+           (adapters.out.purchase-list-category-events/->CategoryCreatedEvent category (misc.date/numb-now) (misc.general/squuid)))
+  category)
 
