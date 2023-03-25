77c88a301f5f1c9268a188dc009e512fd9cca2be
gumberss <gustavomarinsuppi@gmail.com>
2023-03-25 15:04:07 -0300

    fix: tests

--- src/purchase_listinator/modules/events/core.clj
+++ src/purchase_listinator/modules/events/core.clj
@@ -12,20 +12,36 @@
   {:shopping-events/main-db  (component/using (components.datahike/new-datahike :shopping-events/main-db) [:config])
    :shopping-events/rabbitmq (component/using (rabbitmq/new-rabbit-mq :shopping-events/rabbitmq) rabbitmq-dependencies)})
 
+(def system-components-test
+  {:shopping-events/main-db  (component/using (components.datahike/new-datahike :shopping-events/main-db) [:config])
+   :shopping-events/rabbitmq (component/using (rabbitmq/new-rabbit-mq-fake) rabbitmq-dependencies)})
+
+
 (def system-config
   {:shopping-events/main-db  {:store (if (System/getenv "DATAHIKE_PATH")
-                              {:backend :file
-                               :path    (or (System/getenv "DATAHIKE_PATH") "")}
-                              {:backend :mem
-                               :id      "shopping-events/database"})}
+                                       {:backend :file
+                                        :path    (or (System/getenv "DATAHIKE_PATH") "")}
+                                       {:backend :mem
+                                        :id      "shopping-events/database"})}
    :shopping-events/rabbitmq {:host     (or (System/getenv "RABBITMQ_HOST") "localhost")
-                     :port     5672
-                     :username "guest"
-                     :password "guest"
-                     :vhost    "/"}})
+                              :port     5672
+                              :username "guest"
+                              :password "guest"
+                              :vhost    "/"}})
+
+(defn config-test
+  []
+  {:shopping-events/main-db  {:store {:backend :mem
+                                      :id      (str (random-uuid))}}
+   :shopping-events/rabbitmq {}})
 (def config
   {:rabbitmq-dependencies rabbitmq-dependencies
-   :webapp-dependencies webapp-dependencies
-   :system-components components
-   :system-config system-config
-   :system-config-test system-config})
+   :webapp-dependencies   webapp-dependencies
+   :system-components     components
+   :system-config         system-config})
+
+
+(defn system-config-test
+  []
+  {:system-components system-components-test
+   :system-config     (config-test)})
