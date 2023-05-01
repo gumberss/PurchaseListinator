ca386cd02806bf0ea9e82d806fc1f3177a9fce6f
gumberss <gustavomarinsuppi@gmail.com>
2023-04-30 23:20:47 -0300

    feat: adding coerce to http request result

--- src/purchase_listinator/modules/price_suggestion/components/http.clj
+++ src/purchase_listinator/modules/price_suggestion/components/http.clj
@@ -1,11 +1,9 @@
 (ns purchase-listinator.modules.price-suggestion.components.http
   (:require
-    [camel-snake-kebab.core :as csk]
     [clj-http.client :as client]
-    [clojure.data.json :as json]
     [com.stuartsierra.component :as component]
     [purchase-listinator.misc.general :as misc.general]
-    [schema.coerce :as coerce]
+    [purchase-listinator.misc.content-type-parser :as misc.content-type-parser]
     [schema.core :as s]))
 (def request-data-skeleton
   {:method                         s/Keyword
@@ -40,27 +38,18 @@
   (request [{:keys [routes]}
             {:keys [method url params body content-type result-schema headers user-id]}]
     (let [url-str (url routes)
-          _ (println url-str)
           request-fn (condp = method
                        :get client/get
                        :post client/post
                        :put client/put
                        :delete client/delete)
-
           request-params (misc.general/assoc-some {:as      "UTF-8"
                                                    :headers (assoc headers :user-id user-id)}
                                                   :params params
                                                   :body body
                                                   :content-type content-type)
           {:keys [body] :as _request-result} (request-fn url-str request-params)
-_ (clojure.pprint/pprint body)
-          map-data (json/read-str body :key-fn csk/->kebab-case-keyword)
-          map-data (if (string? map-data)
-                     (json/read-str map-data :key-fn csk/->kebab-case-keyword) ;I don't know why I need to do it the second time
-                     map-data)
-          coerce-function (if result-schema
-                            (coerce/coercer result-schema coerce/json-coercion-matcher) identity)
-          coerced-data (coerce-function map-data)]
+          coerced-data (misc.content-type-parser/json->edn body result-schema)]
       coerced-data)))
 
 (defn new-http []
