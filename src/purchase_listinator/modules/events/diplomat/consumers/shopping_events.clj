63b3049a14c503bb3f96f7e009452f5c4693332d
gumberss <gustavomarinsuppi@gmail.com>
2023-07-15 09:03:25 -0300

    feat: receiving shopping cart closed events in the shopping events module

--- src/purchase_listinator/modules/events/diplomat/consumers/shopping_events.clj
+++ src/purchase_listinator/modules/events/diplomat/consumers/shopping_events.clj
@@ -12,8 +12,19 @@
   (-> (adapters.in.shopping-events/wire->internal event)
       (events.flows.shopping-events/receive-events components)))
 
+(s/defn shopping-cart-events-receive
+  [_channel
+   _metadata
+   components
+   event]
+  (clojure.pprint/pprint event))
+
 (def subscribers
   [{:exchange :purchase-listinator/shopping.finished
     :queue    :purchase-listinator.events/shopping-events-receive
     :schema   wires.in.shopping-finished-event/ShoppingFinishedEvent
-    :handler  shopping-events-received}])
+    :handler  shopping-events-received}
+   {:exchange :shopping-cart/shopping-cart.closed
+    :queue    :purchase-listinator.events/shopping-cart-events.receive
+    ;:schema   wires.in.shopping-finished-event/ShoppingFinishedEvent
+    :handler  shopping-cart-events-receive}])
