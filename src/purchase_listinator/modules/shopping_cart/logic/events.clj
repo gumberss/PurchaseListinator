f08cbd2979355062ec540bc92effadbe9a207942
gumberss <gustavomarinsuppi@gmail.com>
2023-07-30 14:02:38 -0300

    chore: adding schemas

--- src/purchase_listinator/modules/shopping_cart/logic/events.clj
+++ src/purchase_listinator/modules/shopping_cart/logic/events.clj
@@ -3,14 +3,14 @@
     [purchase-listinator.modules.shopping-cart.schemas.internal.cart-events :as internal.cart-events]
     [schema.core :as s]))
 
-(s/defn irrelevant-events-for
+(s/defn irrelevant-events? :- s/Bool
   [shopping-id :- s/Uuid
    {:keys [event-type] :as event} :- internal.cart-events/CartEvent]
   (let [possible-irrelevant-events #{:reorder-category :reorder-item}]
     (and (contains? possible-irrelevant-events event-type)
          (not= shopping-id (:shopping-id event)))))
 
-(s/defn filter-by-shopping
+(s/defn filter-by-shopping :- [internal.cart-events/CartEvent]
   [shopping-id :- s/Uuid
    events :- [internal.cart-events/CartEvent]]
-  (remove (partial irrelevant-events-for shopping-id)) events)
+  (remove (partial irrelevant-events? shopping-id)) events)
